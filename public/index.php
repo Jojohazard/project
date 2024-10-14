5fdf8c13cc83306ac3ed74660a186e3c236f8ad4
jonas <jonasbestnet@gmail.com>
2024-10-14 04:00:59 +0200

    t

--- public/index.php
+++ public/index.php
@@ -7,7 +7,6 @@ $method = $_SERVER['REQUEST_METHOD'];
 if ($router->hasRoutes($req, $method)) {
     call_user_func($router->getRoutes()[$req]->getHandler());
 } else {
-    http_response_code(404);
-    header("Location: /404");
+    header("Location: /404", false , 404);
     exit();
 }
